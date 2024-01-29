505bae19e932d2686b116dca75a9758a345abce2
Alexandre Vernet <alexandre.vernet99@gmail.com>
2024-01-29 21:10:42 +0100

    feat(notification): Remove notification content

--- functions/notification.js
+++ functions/notification.js
@@ -1,20 +1,9 @@
 const admin = require("firebase-admin");
 
 const sendNotification = (token) => {
-    const message = {
-        notification: {
-            title: 'Dring Dring 🕰️💊 !'
-        },
-        android: {
-            notification: {
-                imageUrl: 'https://github.com/Alexandre-Vernet/Bot-Bubulle-API/blob/main/assets/images/icon.png?raw=true'
-            }
-        },
-        token
-    };
+    const message = { token };
 
     admin.messaging().send(message)
-        .then(e => console.log(e))
         .catch((error) => {
             console.error('Error sending message:', error);
         });
